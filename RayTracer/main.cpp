81f1fa2b9ab9e485ecf7f5952391f3acbda74947
Myles Pribeg <mylespribeg@gmail.com>
2021-11-12 01:21:55 -0700

    improvements

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -46,17 +46,19 @@ int main() {
 	const int image_width = 400;
 	const int image_height= static_cast<int>(image_width/aspect_ratio);
 	const int samples_per_pixel = 500;
-	const int max_depth = 3;
+	const int max_depth = 50;
 
 	//World
 	hittable_list world;
 
 	auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-	auto material_centre = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-	auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-	auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+	//auto material_centre = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+	//auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
+	auto material_centre = make_shared<dielectic>(1.5);
+	auto material_left = make_shared<metal>(color(0.3,0.5,0.9), 0.3);
+	auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0);
 
-	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5, material_centre));
+	world.add(make_shared<sphere>(point3(0, 0, -1), -0.5, material_centre));
 	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, material_ground));
 	world.add(make_shared<sphere>(point3(1, 0, -1), 0.5, material_right));
 	world.add(make_shared<sphere>(point3(-1, 0, -1), 0.5, material_left));

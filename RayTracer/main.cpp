62e388ebc12c93fd808b64181fe4a68ab2c2f8b6
Myles Pribeg <mylespribeg@gmail.com>
2022-02-24 21:23:41 -0700

    MC integration on sphere of directions

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -8,24 +8,20 @@
 
 // A NEW MAIN PROGRAM TO EXPLORE MONTE CARLO ALOGRITHMS IN MORE DEPTH
 
-double pdf(double x) {
-	return (3*x*x)/8;
+double pdf(vec3& x) {
+	return 1/(4*pi);
 }
 
 int main() {
-	int N = 1;
+	int N = 1000000;
 	auto sum = 0.0;
-	auto upper = 2;
-	auto lower = 0;
 
 	for (int i = 0; i < N; i++) {
-
-		auto x = pow(random_double(lower, upper), 1.0/3.0);
-		sum += x*x/pdf(x);  //sum += 2 * x^2 / x
+		vec3 d = random_unit_vector();
+		auto cosine_squared = d.z() * d.z();
+		sum += cosine_squared / pdf(d);  //pdf is constant 
 
 	}
-
-	auto range = upper - lower;
 
 	std::cout << std::fixed << std::setprecision(12);
 

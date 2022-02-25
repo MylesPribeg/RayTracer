5df760a79677e9ab126d08451bcaf74240d0db9b
Myles Pribeg <mylespribeg@gmail.com>
2022-02-24 21:02:54 -0700

    MC Integration with PDF

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -5,24 +5,33 @@
 #include <math.h>
 #include <stdlib.h>
 
+
 // A NEW MAIN PROGRAM TO EXPLORE MONTE CARLO ALOGRITHMS IN MORE DEPTH
+
+double pdf(double x) {
+	return 0.5 * x;
+}
 
 int main() {
 	int N = 100000;
 	auto sum = 0.0;
+	auto upper = 4;
+	auto lower = 0.000001;
 
 	for (int i = 0; i < N; i++) {
 
-		auto x = random_double(0, 2);
-		sum += x * x;
+		auto x = sqrt(random_double(lower, upper));
+		sum += x*x/pdf(x);  //sum += 2 * x^2 / x
 
 	}
 
+	auto range = upper - lower;
+
 	std::cout << std::fixed << std::setprecision(12);
 
-	std::cout << "I = " << 2 * sum/N << std::endl;
+	std::cout << "I = " << /*range * */ sum / N << std::endl; // from Mean Value Theorem
+	// integral(x) from a to b = f(c) * (b - a) where f(c) is the average value
 
 
 	return 0;
 }
-
